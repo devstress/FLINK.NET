4913b5a4ebd8e021c6e00228c23b76ca2d0d9f9d
Darren Dat Bui <30769729+devstress@users.noreply.github.com>
2025-06-10 23:15:46 +1000

    Simplify integration test image build

--- IntegrationTestImage/entrypoint.sh
+++ IntegrationTestImage/entrypoint.sh
@@ -9,12 +9,17 @@ until docker info >/dev/null 2>&1; do
   sleep 1
 done
 
-# Load cached images
+# Load cached images or pull if missing
 if [ -f /redis.tar ]; then
   docker load -i /redis.tar || true
+else
+  docker pull redis:latest
 fi
+
 if [ -f /kafka.tar ]; then
   docker load -i /kafka.tar || true
+else
+  docker pull confluentinc/cp-kafka:latest
 fi
 
 exec ./IntegrationTestImage "$@"
