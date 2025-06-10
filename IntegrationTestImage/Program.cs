0541c9811ca029774440308888dfb166a8db7fd0
Darren Dat Bui <30769729+devstress@users.noreply.github.com>
2025-06-10 22:19:49 +1000

    Fix integration test image build

--- IntegrationTestImage/Program.cs
+++ IntegrationTestImage/Program.cs
@@ -1,10 +1,24 @@
+using System;
+using System.Reflection;
 using System.Threading.Tasks;
+
 namespace IntegrationTestImage;
 
 public class Program
 {
     public static async Task Main(string[] args)
     {
-        await FlinkDotNetAspire.AppHost.Program.Main(args);
+        var assembly = Assembly.Load("FlinkDotNetAspire.AppHost");
+        var program = assembly.GetType("Program") ?? assembly.GetType("FlinkDotNetAspire.AppHost.Program");
+        var main = program?.GetMethod("Main", BindingFlags.Static | BindingFlags.Public | BindingFlags.NonPublic);
+        if (main == null)
+        {
+            throw new MissingMethodException("Unable to locate AppHost entry point");
+        }
+        var result = main.Invoke(null, new object[] { args });
+        if (result is Task task)
+        {
+            await task.ConfigureAwait(false);
+        }
     }
 }
